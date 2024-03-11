b61a705a6716e9694a056717df9c871bac38b508
priyanshu3424 <140826461+priyanshu3424@users.noreply.github.com>
2024-03-11 23:22:28 +0530

    Add files via upload

---
+++ counting.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+//Function signature
+void printCounting(int n){
+    //Function body
+    for(int i=1;i<=n;i++){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    //Function call
+    printCounting(n);
+
+    return 0;
+}
