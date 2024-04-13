87d652a1ad1cbb1aa1334e316406db7f54709b5c
priyanshu3424 <140826461+priyanshu3424@users.noreply.github.com>
2024-04-13 23:29:29 +0530

    Update decimaltobinary.cpp

--- decimaltobinary.cpp
+++ decimaltobinary.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
-
 int main(){
 
     int n;
@@ -12,13 +11,10 @@ int main(){
     while(n!=0){
 
         int bit= n & 1;
-
         ans=(bit*pow(10,i) ) +ans;
 
         n=n >> 1;
         i++;
     }
-
     cout<<"Answer is "<<ans<<endl;
-
 }
