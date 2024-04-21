49401648c788f8294252c25f2dc6bc3040ef56d7
Priyanshu Jain <140826461+priyanshu3424@users.noreply.github.com>
2024-04-21 19:03:45 +0530

    Update pointer3.cpp

--- pointer3.cpp
+++ pointer3.cpp
@@ -1,13 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(){
-    //int num =5;
-    // cout<<num<<endl;
-
-    // //address of operator - &
-    // cout<<"Address of num is "<<&num <<endl;
-
-    //int *ptr= & num;
 
     double d=4.3;
     double *p2 =&d;
