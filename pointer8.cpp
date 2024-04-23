cba3df1ce9df841fbaac651bf341715df5bd30d2
Priyanshu Jain <140826461+priyanshu3424@users.noreply.github.com>
2024-04-23 23:15:53 +0530

    Update pointer8.cpp

--- pointer8.cpp
+++ pointer8.cpp
@@ -2,11 +2,20 @@
 using namespace std;
 
 int main(){
-    int arr[10]={2,5,6};
+    //int arr[10]={2,5,6};
+    int arr[10]={23,122,41,67};
 
     cout<<arr[0]<<endl;
 
     cout<<"4th: "<< *arr<<endl;
+    cout<<"5th: "<<*arr+1 <<endl;
+    cout<<"6th: "<<*(arr+1)<<endl;
+    cout<<"7th: "<<*(arr)+1<<endl;
+    cout<<"8th: "<<arr[2]<<endl;
+    cout<<"9th: "<<*(arr+2)<<endl;
+
+    int i=3;
+    cout<<i[arr]<<endl;
 
     return 0;
 }
