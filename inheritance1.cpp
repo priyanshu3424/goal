6fd17e3245a0acfd66901bc5b0fd064ba5b48098
Priyanshu Jain <140826461+priyanshu3424@users.noreply.github.com>
2024-05-18 01:11:48 +0530

    Add files via upload

---
+++ inheritance1.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Human{
+
+    public:
+    int height;
+    int weight;
+    int age;
+
+    public:
+    int getAge(){
+        return this->age;
+    }
+    void setweight(int w){
+        this->weight =w;
+    }
+
+};
+
+class Male: public Human{
+
+    public:
+    string color;
+
+    void sleep(){
+        cout<<"Male sleeping"<<endl;
+    }
+};
+int main()
+{
+    Male object1;
+    cout<<object1.age<<endl;
+    cout<<object1.weight<<endl;
+    cout<<object1.height<<endl;
+
+    cout<<object1.color<<endl;
+
+    object1.setweight(55);
+    cout<<object1.weight<<endl;
+    object1.sleep();
+
+    return 0;
+}
