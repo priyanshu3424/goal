21815ad4f6e7b0afd73acdf34f109195698629cf
Priyanshu Jain <140826461+priyanshu3424@users.noreply.github.com>
2024-05-18 00:35:37 +0530

    Add files via upload

---
+++ encapsulation.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student {
+    string name;
+    int age;
+    int height;
+    
+    public:
+    int getAge() { return age; }
+};
+
+int main() {
+    Student first;
+    cout << "Sab changa h" << endl;
+    return 0;
+}
