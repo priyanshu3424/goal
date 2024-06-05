64d1b6d6e27b6ce5c6c46083b7e81b1acfc0bb73
Priyanshu Jain <140826461+priyanshu3424@users.noreply.github.com>
2024-06-05 23:20:05 +0530

    Add files via upload

---
+++ recursion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void pArray(int* arr, int n)
+{
+    if (n == 0) {
+        return;
+    }
+    // recursive call
+    cout << arr[n - 1] << ' ';
+    pArray(arr, n - 1);  
+}
+
+int main()
+{
+    int arr[5] = { 1, 2, 3, 4, 5 };
+    pArray(arr, 5);
+    return 0;
+}
